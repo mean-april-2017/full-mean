33f8de17f9079de4abffe7efc492b2a519d59ebd
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-12 18:19:00 -0500

    ng-route between partials

--- client/app.js
+++ client/app.js
@@ -1,1 +1,11 @@
 var app = angular.module("itemsApp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/new", {
+        templateUrl: "/partials/new-item.html"
+    })
+    .when("/index", {
+        templateUrl: "/partials/items-index.html"
+    })
+    .otherwise("/index");
+});
