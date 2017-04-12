b66ba6d288763d48c243843b47b19b6abbb4fa7d
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-12 13:47:17 -0500

    can save test item

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -2,3 +2,20 @@
     Handle Incoming Requests for Items
 */
 console.log("/server/controllers/items.js");
+
+var mongoose = require("mongoose");
+var Item = mongoose.model("Item");
+
+
+// Test Creating Item
+var item = new Item({
+    title: "Test Item",
+    dueDate: new Date()
+});
+item.save(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully Created Item!", item);
+    }
+});
--- server/models/item.js
+++ server/models/item.js
@@ -2,3 +2,10 @@
     Item DB Model
 */
 console.log("/server/models/item.js");
+
+var mongoose = require("mongoose");
+var itemSchema = mongoose.Schema({
+    title: String,
+    dueDate: Date
+}, { timestamps: true });
+mongoose.model("Item", itemSchema);
