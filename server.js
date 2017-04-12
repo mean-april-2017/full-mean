0b533e3fe9fabb20c989c787032752bfebe1c1bf
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-12 14:28:55 -0500

    Routes Files

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
     Main Server File
 */
 console.log("/server.js");
+var express = require("express");
+var app = express();
 
 require("./server/config/database");
-require("./server/config/routes");
+require("./server/config/routes")(app);
+
+app.listen(3000, function () {
+    console.log("Listening on Port 3000");
+});
--- server/config/routes.js
+++ server/config/routes.js
@@ -3,3 +3,9 @@
 */
 console.log("/server/config/routes.js");
 require("../controllers/items");  // Require Items Controller
+
+module.exports = function (app)
+{
+    app.get("/api/items", function () { console.log("items index"); })
+    app.post("/api/items", function () { console.log("items create"); })
+}
