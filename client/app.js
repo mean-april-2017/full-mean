367fa853f582d6571a824b88dfcfebec6344e27b
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-12 18:25:09 -0500

    controllers synced with html

--- client/app.js
+++ client/app.js
@@ -2,10 +2,25 @@ var app = angular.module("itemsApp", ["ngRoute"]);
 app.config(function ($routeProvider) {
     $routeProvider
     .when("/new", {
-        templateUrl: "/partials/new-item.html"
+        templateUrl: "/partials/new-item.html",
+        controller: "newItemController"
     })
     .when("/index", {
-        templateUrl: "/partials/items-index.html"
+        templateUrl: "/partials/items-index.html",
+        controller: "itemsIndexController"
     })
     .otherwise("/index");
+});
+app.controller("itemsIndexController", function ($scope) {
+    $scope.items = [
+        { title: "Do Stuff", dueDate: new Date() },
+        { title: "Do More Stuff", dueDate: new Date() },
+        { title: "Do Even More Stuff", dueDate: new Date() },
+    ];
+});
+app.controller("newItemController", function ($scope) {
+    $scope.addItem = function () {
+        console.log("NEW ITEM:", $scope.item);
+        $scope.item = {};
+    }
 });
