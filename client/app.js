832224f5b9b7c1275952fb9627fdf10e17bb213f
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-12 18:27:50 -0500

    controllers save data in factory

--- client/app.js
+++ client/app.js
@@ -11,16 +11,30 @@ app.config(function ($routeProvider) {
     })
     .otherwise("/index");
 });
-app.controller("itemsIndexController", function ($scope) {
-    $scope.items = [
+app.factory("itemFactory", function () {
+    var factory = {};
+    var items = [
         { title: "Do Stuff", dueDate: new Date() },
         { title: "Do More Stuff", dueDate: new Date() },
         { title: "Do Even More Stuff", dueDate: new Date() },
     ];
+
+    factory.addItem = function (item) {
+        items.push(item);
+    }
+    factory.allItems = function () {
+        return items;
+    }
+
+    return factory;
+})
+app.controller("itemsIndexController", function ($scope, itemFactory) {
+    $scope.items = itemFactory.allItems();
 });
-app.controller("newItemController", function ($scope) {
+app.controller("newItemController", function ($scope, itemFactory) {
     $scope.addItem = function () {
         console.log("NEW ITEM:", $scope.item);
+        itemFactory.addItem($scope.item);
         $scope.item = {};
     }
 });
